deef09b6d89e66cc98081f9dbe3836bb76e9ac0e
WerqetTPETb <alextret@gmail.com>
2013-11-13 17:05:09 +0300

    new icons hotel page slight changes

--- config.rb
+++ config.rb
@@ -7,12 +7,13 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 generated_images_dir = "sprites"
+http_generated_images_path = "/sprites"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-relative_assets = true
+# relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
